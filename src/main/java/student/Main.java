8bfe5ca41e084efd1e7ae92f72aa00d93f6b923d
Ellen Spertus <ellen.spertus@gmail.com>
2023-07-25 13:05:23 -0700

    Add checks for forbidden parse nodes

--- src/main/java/student/Main.java
+++ src/main/java/student/Main.java
@@ -1,10 +1,13 @@
 package student;
 
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.stmt.ForEachStmt;
 import com.spertus.jacquard.checkstylegrader.CheckstyleGrader;
 import com.spertus.jacquard.common.*;
 import com.spertus.jacquard.junittester.JUnitTester;
 import com.spertus.jacquard.pmdgrader.PmdGrader;
 import com.spertus.jacquard.publisher.GradescopePublisher;
+import com.spertus.jacquard.syntaxgrader.SyntaxConditionGrader;
 
 import java.util.*;
 
@@ -30,6 +33,34 @@ public class Main {
                 "category/java/bestpractices.xml",
                 "MissingOverride");
         results.addAll(pmdGrader.grade(target));
+
+        // Make sure students did not use enhanced for loops.
+        SyntaxConditionGrader forGrader = new SyntaxConditionGrader(
+                0,
+                "enhanced for-loop",
+                5.0,
+                node -> (node instanceof ForEachStmt)
+        );
+        results.addAll(forGrader.grade(target));
+
+        // Make sure students did not call other implementations of
+        // hasNext() or next()
+        List<String> methodNames = List.of("hasNext", "next");
+        SyntaxConditionGrader nextGrader = new SyntaxConditionGrader(
+                0,
+                "calls of other class's iterator methods",
+                5.0,
+                node -> {
+                    // Check if method call is to next() or hasNext().
+                    if (node instanceof MethodCallExpr methodCallExpr &&
+                            methodNames.contains(methodCallExpr.getNameAsString())) {
+                        // If so, flag if scope is explicit and not "this".
+                        return methodCallExpr.getScope().isPresent() &&
+                                !(methodCallExpr.getScope().get() instanceof ThisExpr);
+                    }
+                    return false;
+                });
+        results.addAll(nextGrader.grade(target));
 
         // Run unit tests.
         JUnitTester runner = new JUnitTester(HiddenFavoriteIteratorsTest.class, ProvidedFavoritesIteratorTest.class);

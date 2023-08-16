91f0992055b748036a202db14a494f885f23abce
Ellen Spertus <ellen.spertus@gmail.com>
2023-08-16 13:14:04 -0700

    Fix autograder upload error

--- quiz1/setup.sh
+++ quiz1/setup.sh
@@ -9,7 +9,7 @@ mkdir -p lib
 wget -nc -P lib https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.1/checkstyle-10.12.1-all.jar
 
 # This fixes carriage returns in scripts.
-dos2unix run_autograder run_autograder.py
+dos2unix run_autograder run_autograder.py gradlew
 
 # This runs Gradle once, to download any needed files.
 ./gradlew clean
